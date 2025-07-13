e7a25c195dcd9b23c10806b706d228c8bac92847
KIM69ZS28 <kz.kmt6928@gmail.com>
2025-07-13 23:43:24 +0900

    サブネットの追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,14 @@ resource "aws_vpc" "pf_vpc" {
     }
 
 }
+
+resource "aws_subnet" "pf_subnet" {
+
+    vpc_id = aws_vpc.pf_vpc.id
+
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "pf_app_subnet"
+    }
+  
+}
